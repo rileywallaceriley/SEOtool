74299f76cf6012a365aad9b588a2b9d2d9e47c90
rileywallaceriley <67809682+rileywallaceriley@users.noreply.github.com>
2024-02-16 16:29:37 -0500

    Update Writer.py

--- Writer.py
+++ Writer.py
@@ -79,8 +79,26 @@ if content_generated:  # This block will only execute if blog content has been s
 
     # Tool descriptions
     tools = [
-        # Your tools definitions here
-    ]
+    {
+        "header": "RepuSEO-Helper",
+        "description": "Receive personalized SEO recommendations to improve your site's ranking and user experience.",
+        "button_label": "Use Now - RepuSEO-Helper",
+        "button_url": "https://seotool-qpb8fq8bygcusdsxn6pm6s.streamlit.app"
+    },
+    {
+        "header": "Competitive Edge",
+        "description": "Analyze and apply winning SEO strategies from your competitors directly into your campaign.",
+        "button_label": "Use Now - Competitive Edge",
+        "button_url": "https://seotool-mfvdnqmf32f3visjegsxho.streamlit.app"
+    },
+    {
+        "header": "RepuSEO Plagiarism Checker",
+        "description": "Ensure the originality of your content with our advanced plagiarism detection tool.",
+        "button_label": "Use Now - RepuSEO Plagiarism Checker",
+        "button_url": "https://seotool-cdjzyqj4qrskqvkuahwjwm.streamlit.app"
+    },
+    
+]
 
     # Loop through each tool and display its section
     for tool in tools:
